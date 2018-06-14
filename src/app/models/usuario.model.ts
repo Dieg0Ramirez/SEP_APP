12b92fba5219c8b6088cd19ed9c64b8604f7757e
Dieg0Ramirez <37415207+Dieg0Ramirez@users.noreply.github.com>
2018-06-14 13:03:20 -0500

    Update usuario.model.ts
    modificar modelo

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -4,7 +4,7 @@ export class Usuario  {
         public nombre: string,
         public email: string,
         public password: string,
-        public rol: string,
+        public rol?: string,
         public disponible?: boolean,
         public _id?: string
     ) { }
