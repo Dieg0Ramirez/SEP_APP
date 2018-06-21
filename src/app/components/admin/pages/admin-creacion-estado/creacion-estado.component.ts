653d71270dcb523ac45e41d68ea4b52718ca821f
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-06-21 15:35:21 -0500

    correcion de progromas.html y actualizacion de estado

--- src/app/components/admin/pages/admin-creacion-estado/creacion-estado.component.ts
+++ src/app/components/admin/pages/admin-creacion-estado/creacion-estado.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { EstadoService } from '../../../../services/services.index';
 import { Router } from '@angular/router';
 import { Estado } from '../../../../models/estado.models';
 import {FormGroup, FormControl, Validators } from '@angular/forms';
 import { spanish } from '../../../../interfaces/dataTables.es';
 import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
 
 declare var AdminLTE: any;
 
@@ -13,7 +14,10 @@ declare var AdminLTE: any;
   templateUrl: './creacion-estado.component.html',
   styleUrls: ['./creacion-estado.component.css']
 })
-export class CreacionEstadoComponent implements OnInit {
+export class CreacionEstadoComponent implements OnInit, OnDestroy {
+
+  @ViewChild (DataTableDirective) dtElement: DataTableDirective;
+
   forma: FormGroup;
   dtOptions: any = {};
 
@@ -48,14 +52,6 @@ export class CreacionEstadoComponent implements OnInit {
       ]
     };
 
-    this._estadoService.listarEstado().subscribe(res => {
-
-      console.log(res);
-
-      this.estado = res.data;
-      this.dtTrigger.next();
-    });
-
     this.cargarEstados();
 
     this.forma = new FormGroup({
@@ -68,18 +64,22 @@ export class CreacionEstadoComponent implements OnInit {
 
       console.log(res);
       this.estado = res.estados;
+      this.dtTrigger.next();
 
     });
   }
 
   actualizarEstado() {
+    const estado = new Estado(
+      this.forma.value.nombreestado
+    );
+    this._estadoService.actualizarEstado(estado).subscribe(() => {
 
-  const estado = new Estado(
-    this.forma.value.nombreestado
-  );
-  this._estadoService.actualizarEstado( estado )
-        .subscribe( resp => this.router.navigate(['admin/admin-creacionEstado']) );
-
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        this.cargarEstados();
+      });
+    });
   }
 
   registrarEstado() {
@@ -87,9 +87,18 @@ export class CreacionEstadoComponent implements OnInit {
       this.forma.value.nombreestado
     );
 
-    this._estadoService.crearEstado( estado )
-              .subscribe( resp => this.router.navigate(['/admin/admin-creacionEstado']) );
+    this._estadoService.crearEstado(estado).subscribe(() => {
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        this.cargarEstados();
+      });
+    });
 
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
 
 }
