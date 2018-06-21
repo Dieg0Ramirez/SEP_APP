c8227cc2d780008926abdc6831bc6b63af892036
andres <andres_t9@outlook.com>
2018-06-21 16:37:23 -0500

    Logo, Tables y excel

--- src/app/components/admin/pages/admin-excel-juicios-evaluativos/admin-excel-juicios-evaluativos.component.ts
+++ src/app/components/admin/pages/admin-excel-juicios-evaluativos/admin-excel-juicios-evaluativos.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormControlName } from '@angular/forms';
+import { NivelFormacionService } from '../../../../services/services.index';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { spanish } from '../../../../interfaces/dataTables.es';
+import { DataTableDirective } from 'angular-datatables';
+import { NivelFormacion } from '../../../../models/nivelFormacion.models';
+
 declare var AdminLTE: any;
 
 @Component({
@@ -6,12 +14,53 @@ declare var AdminLTE: any;
   templateUrl: './admin-excel-juicios-evaluativos.component.html',
   styleUrls: ['./admin-excel-juicios-evaluativos.component.css']
 })
-export class AdminExcelJuiciosEvaluativosComponent implements OnInit {
+export class AdminExcelJuiciosEvaluativosComponent implements OnInit, OnDestroy {
+  @ViewChild (DataTableDirective) dtElement: DataTableDirective;
+  forma: FormGroup;
+  dtOptions: any = {};
 
-  constructor() { }
+  dtLanguage: any = spanish;
+  dtTrigger: Subject<any> = new Subject();
+
+  nivelFormacion: NivelFormacion[] = [];
+
+  constructor(
+    public _nivelFormacionServices: NivelFormacionService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
     AdminLTE.init();
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      language: this.dtLanguage,
+      // Declare the use of the extension in the dom parameter
+      dom: 'lfBrtip',
+
+      // Configure the buttons
+      buttons: [
+        { extend: 'colvis', text: 'Ocultar/Mostrar Columnas' },
+        {
+          extend: 'copy', text: 'Copiar al portapapeles'
+        },
+        { extend: 'print', text: 'Imprimir' },
+        { extend: 'excel', text: 'Exportar a Excel' },
+      ]
+    };
+
+    this._nivelFormacionServices.listarNivelFormacion().subscribe((res: any) => {
+
+      console.log(res);
+      this.nivelFormacion = res.nivelFormacion;
+      this.dtTrigger.next();
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
 
 }
