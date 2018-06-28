f95dfcdabdecbc9a5475a13aef3fa26c1e97f0e0
Diego <dochoa743@gmail.com>
2018-06-28 15:11:29 -0500

    exportacion de servicio de fichas

--- src/app/services/services.index.ts
+++ src/app/services/services.index.ts
@@ -8,10 +8,5 @@ export { ProgramaService } from './programa/programa.service';
 export { LoginGuardGuard } from './guards/login-guard.guard';
 export { UsuarioService } from './usuario/usuario.service';
 export { TipoDocumentoService } from './tipoDocumento/tipo-documento.service';
-<<<<<<< HEAD
 export { FichasService } from './fichas/fichas.service';
-
-
-=======
 export { ExcelService } from './excel/excel.service';
->>>>>>> afbb5e3ac0506c6baf3760f963b188384d4a25a3
